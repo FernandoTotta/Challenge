26303fa406e437fa82f9d6b7462f01230773354f
Fernando Vieira <f-vieira1@hotmail.com>
2022-08-18 12:08:47 -0300

    Base-layout - Included base files

---
+++ Challenge/News/View/NewsView.swift
@@ -0,0 +1,12 @@
+//
+//  NewsView.swift
+//  Challenge
+//
+//  Created by Fernando on 18/08/22.
+//
+
+import UIKit
+
+class NewsView: UIView {
+
+}
---
+++ Challenge/News/View/NewsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NewsViewController.swift
+//  Challenge
+//
+//  Created by Fernando on 18/08/22.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
